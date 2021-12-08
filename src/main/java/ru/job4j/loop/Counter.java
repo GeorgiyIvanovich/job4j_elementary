e76f92fcbe5a1b109b8a34d2838bf4611bb38e72
Георгий <gkarkazidi@gmail.com>
2021-12-08 15:00:00 +0300

    Сумма чисел. [#226416 #237168]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -8,6 +8,7 @@ public class Counter {
         }
         return sum;
     }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(4, 12));
