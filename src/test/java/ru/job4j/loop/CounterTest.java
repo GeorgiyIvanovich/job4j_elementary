f8393c805784210f53d3dba64465f284b6cd32ff
Георгий <gkarkazidi@gmail.com>
2021-12-12 22:02:23 +0300

    исправление

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 public class CounterTest {
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
