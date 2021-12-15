596bd3e4f07a7cb0702d7742d509f3a2fc64188b
Георгий <gkarkazidi@gmail.com>
2021-12-15 17:06:05 +0300

    Перевернуть массив. [#226790 #240012]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,9 +2,6 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        if (array == null || array.length <= 1) {
-            return null;
-        }
             for (int index = 0; index < array.length / 2; index++) {
                 int temp = array[index];
                 array[index] = array[array.length - 1 - index];
--- src/test/java/ru/job4j/array/TurnTest.java
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -9,6 +9,7 @@ public class TurnTest {
     @Test
     public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
         int[] input = new int[] {4, 1, 6, 2};
+
         int[] result = Turn.back(input);
         int[] expected = new int[] {2, 6, 1, 4};
         Assert.assertArrayEquals(expected, result);
